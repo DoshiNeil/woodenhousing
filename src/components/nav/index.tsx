25c3a88be6bc8f65b247725ed6d63ce2051dc4dd
Neil Doshi <mail.neildoshi@gmail.com>
2024-11-16 17:57:09 +0530

    feat: add v0 of navigation menu

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,10 +1,61 @@
+import Link from "next/link";
 import { Button } from "../common/button";
 import Logo from "../common/Logo";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "../common/navigation-menu";
 
 export default function Navbar() {
   return (
     <nav className="flex flex-1 justify-between">
       <Logo />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem className="font-lexend text-lg font-normal hover:font-bold leading-snug">
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="font-lexend text-lg font-normal leading-snug">
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Design
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="font-lexend text-lg font-normal leading-snug">
+            <NavigationMenuTrigger>Services</NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="font-lexend text-lg font-normal leading-snug">
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About Us
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="font-lexend text-lg font-normal leading-snug">
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Blog
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="font-lexend text-lg font-normal leading-snug">
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Career
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <Button className="text-lg font-bold p-6 rounded-md"> Contact Us </Button>
     </nav>
   );
