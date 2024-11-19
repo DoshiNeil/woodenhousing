0a5325fb1d71fc3a397dd11183421ef4f1ca52b8
Neil Doshi <mail.neildoshi@gmail.com>
2024-11-19 22:09:10 +0530

    feat: add the sidebar base code

---
+++ src/components/nav/Sidebar/index.tsx
@@ -0,0 +1,205 @@
+export default function Sidebar() {
+  return (
+    <div className="w-[347px] h-[876.14px] px-[22px] py-[25px] bg-white flex-col justify-start items-end gap-[15px] inline-flex z-50">
+      <div className="justify-center items-center gap-3 inline-flex">
+        <div className="text-center text-[#3a2d41] text-base font-medium font-['Lexend Deca']">
+          close
+        </div>
+        <div className="w-[15px] h-[15px] relative"></div>
+      </div>
+      <div className="self-stretch h-[757px] flex-col justify-start items-end gap-2.5 flex">
+        <div className="w-[302px] h-[81px] relative">
+          <div className="w-[94px] h-[81px] px-[23.50px] py-[15px] left-0 top-0 absolute bg-[#fff1ec] rounded-lg flex-col justify-center items-center gap-2.5 inline-flex">
+            <div className="w-[47px] text-center text-[#282828] text-[11px] font-normal font-['Lexend Deca']">
+              Home
+            </div>
+          </div>
+          <div className="w-[94px] h-[81px] px-3.5 py-[15px] left-[104px] top-0 absolute bg-[#fff1ec] rounded-lg flex-col justify-center items-center gap-2.5 inline-flex">
+            <div className="w-[66px] text-center text-[#282828] text-[11px] font-normal font-['Lexend Deca']">
+              Contact Us
+            </div>
+          </div>
+          <div className="w-[94px] h-[81px] px-[23.50px] py-[15px] left-[208px] top-0 absolute bg-[#fff1ec] rounded-lg flex-col justify-center items-center gap-[9px] inline-flex">
+            <div className="w-[47px] text-center text-[#282828] text-[11px] font-normal font-['Lexend Deca']">
+              FAQ
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[360px] p-2.5 bg-[#fff1ec] rounded-lg flex-col justify-start items-start gap-2.5 flex">
+          <div className="self-stretch justify-start items-center gap-2.5 inline-flex">
+            <div className="text-center text-[#282828] text-sm font-medium font-['Lexend Deca']">
+              Services
+            </div>
+          </div>
+          <div className="self-stretch h-[312px] flex-col justify-start items-start gap-2.5 flex">
+            <div className="self-stretch h-56 flex-col justify-start items-start gap-1.5 flex">
+              <div className="self-stretch justify-start items-start gap-1.5 inline-flex">
+                <div className="grow shrink basis-0 self-stretch rounded-[5px] shadow flex-col justify-start items-start inline-flex">
+                  <img
+                    className="self-stretch h-[69px] rounded-tl-[5px] rounded-tr-[5px] border border-white"
+                    src="https://via.placeholder.com/138x69"
+                  />
+                  <div className="self-stretch h-10 py-1.5 bg-white justify-center items-center gap-2.5 inline-flex">
+                    <div className="text-center text-[#191919] text-[11px] font-normal font-['Lexend Deca']">
+                      Wooden Cottages
+                    </div>
+                  </div>
+                </div>
+                <div className="grow shrink basis-0 rounded-[5px] shadow flex-col justify-start items-start inline-flex">
+                  <img
+                    className="self-stretch h-[69px] rounded-tl-[5px] rounded-tr-[5px] border border-white"
+                    src="https://via.placeholder.com/138x69"
+                  />
+                  <div className="self-stretch h-10 py-1.5 bg-white justify-center items-center gap-2.5 inline-flex">
+                    <div className="text-center text-[#191919] text-[11px] font-normal font-['Lexend Deca']">
+                      Wooden Villas
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="self-stretch justify-start items-start gap-1.5 inline-flex">
+                <div className="grow shrink basis-0 rounded-[5px] shadow flex-col justify-start items-start inline-flex">
+                  <img
+                    className="self-stretch h-[69px] rounded-tl-[5px] rounded-tr-[5px] border border-white"
+                    src="https://via.placeholder.com/138x69"
+                  />
+                  <div className="self-stretch h-10 py-1.5 bg-white justify-center items-center gap-2.5 inline-flex">
+                    <div className="text-center text-[#191919] text-[11px] font-normal font-['Lexend Deca']">
+                      Wooden Pergola
+                    </div>
+                  </div>
+                </div>
+                <div className="grow shrink basis-0 self-stretch rounded-[5px] shadow flex-col justify-start items-start inline-flex">
+                  <img
+                    className="self-stretch h-[69px] rounded-tl-[5px] rounded-tr-[5px] border border-white"
+                    src="https://via.placeholder.com/138x69"
+                  />
+                  <div className="self-stretch h-10 py-1.5 bg-white justify-center items-center gap-2.5 inline-flex">
+                    <div className="text-center text-[#191919] text-[11px] font-normal font-['Lexend Deca']">
+                      Wooden Cottages
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="self-stretch p-2.5 bg-white rounded-[5px] shadow justify-between items-center inline-flex">
+              <div className="text-center text-[#282828] text-[11px] font-normal font-['Lexend Deca']">
+                Our Projects
+              </div>
+            </div>
+            <div className="self-stretch p-2.5 bg-white rounded-[5px] shadow justify-between items-center inline-flex">
+              <div className="text-center text-[#282828] text-[11px] font-normal font-['Lexend Deca']">
+                Why Wooden House?
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[303px] h-[66px] relative bg-gradient-to-b from-[#f8f1fc] to-[#ebdcf4] rounded-md">
+          <div className="w-[111.28px] h-[125.03px] left-[207px] top-[-7px] absolute">
+            <div className="w-[108.69px] h-[84.12px] left-[2.59px] top-0 absolute"></div>
+            <div className="w-[43.07px] h-[87.77px] left-[30.91px] top-[14.57px] absolute">
+              <div className="opacity-20 w-[16.43px] h-[1.37px] left-[9.26px] top-[55.90px] absolute"></div>
+            </div>
+            <div className="w-[15.38px] h-[17.46px] left-[0.91px] top-[40.14px] absolute"></div>
+            <div className="w-[42.69px] h-[83px] left-[8.58px] top-[41.91px] absolute"></div>
+          </div>
+          <div className="left-[13px] top-[7px] absolute">
+            <span className="text-neutral-800 text-[13px] font-normal font-['Lexend Deca']">
+              Book a
+            </span>
+            <span className="text-[#d75438] text-[13px] font-normal font-['Lexend Deca']">
+              {" "}
+            </span>
+            <span className="text-[#6d2794] text-[13px] font-semibold font-['Lexend Deca']">
+              free
+            </span>
+            <span className="text-neutral-800 text-[13px] font-normal font-['Lexend Deca']">
+              {" "}
+              design consulation!
+            </span>
+          </div>
+          <div className="left-[13px] top-[25px] absolute">
+            <span className="text-[#6c557b] text-[10px] font-light font-['Lexend Deca']">
+              Meet our{" "}
+            </span>
+            <span className="text-[#6c557b] text-[10px] font-normal font-['Lexend Deca']">
+              architect
+            </span>
+            <span className="text-[#6c557b] text-[10px] font-light font-['Lexend Deca']">
+              {" "}
+              & start planning!{" "}
+            </span>
+          </div>
+          <div className="py-[3px] left-[13px] top-[40px] absolute rounded justify-center items-center gap-[5px] inline-flex">
+            <div className="text-[#6d2793] text-[11px] font-medium font-['Lexend Deca']">
+              Book now{" "}
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[42px] p-2.5 bg-[#fff1ec] rounded-[5px] justify-between items-center inline-flex">
+          <div className="justify-start items-center gap-[7px] flex">
+            <div className="w-[22px] h-[22px] relative">
+              <div className="w-[22px] h-[22px] left-0 top-0 absolute bg-[#fff1ec] rounded-full"></div>
+              <div className="w-[13.12px] h-3 left-[4px] top-[5px] absolute">
+                <div className="w-[5.22px] h-[5.22px] left-[4.69px] top-[-0px] absolute rounded-full border border-[#d75438]"></div>
+              </div>
+            </div>
+            <div className="text-center text-[#282828] text-xs font-normal font-['Lexend Deca']">
+              Refer a friend
+            </div>
+            <div className="px-[5px] py-[3px] bg-[#d75438] rounded-[3px] justify-center items-center gap-2.5 flex">
+              <div className="text-center text-white text-[7px] font-normal font-['Lexend Deca']">
+                ₹50,000 Cashback
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[168px] rounded-[5px] flex-col justify-start items-start flex">
+          <div className="self-stretch p-2.5 bg-[#fff1ec] border-b border-[#f4d5ca] justify-between items-center inline-flex">
+            <div className="justify-start items-center gap-[7px] flex">
+              <div className="w-[22px] h-[22px] relative">
+                <div className="w-[22px] h-[22px] left-0 top-0 absolute bg-[#fff1ec] rounded-full"></div>
+                <div className="w-3.5 h-[14.50px] left-[4px] top-[4px] absolute"></div>
+              </div>
+              <div className="text-center text-[#282828] text-xs font-normal font-['Lexend Deca']">
+                Designs
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch p-2.5 bg-[#fff1ec] border-b border-[#f4d5ca] justify-between items-center inline-flex">
+            <div className="justify-start items-center gap-[7px] flex">
+              <div className="w-[22px] h-[22px] relative">
+                <div className="w-[22px] h-[22px] left-0 top-0 absolute bg-[#fff1ec] rounded-full"></div>
+              </div>
+              <div className="text-center text-[#282828] text-xs font-normal font-['Lexend Deca']">
+                About
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch p-2.5 bg-[#fff1ec] border-b border-[#f4d5ca] justify-between items-center inline-flex">
+            <div className="justify-start items-center gap-[7px] flex">
+              <div className="w-[22px] h-[22px] relative">
+                <div className="w-[22px] h-[22px] left-0 top-0 absolute bg-[#fff1ec] rounded-full"></div>
+              </div>
+              <div className="text-center text-[#282828] text-xs font-normal font-['Lexend Deca']">
+                Career
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch p-2.5 bg-[#fff1ec] justify-between items-center inline-flex">
+            <div className="justify-start items-center gap-[7px] flex">
+              <div className="w-[22px] h-[22px] relative">
+                <div className="w-[22px] h-[22px] left-0 top-0 absolute bg-[#fff1ec] rounded-full"></div>
+                <div className="w-[10.80px] h-[13.50px] left-[6px] top-[4px] absolute"></div>
+              </div>
+              <div className="text-center text-[#282828] text-xs font-normal font-['Lexend Deca']">
+                Blog
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="self-stretch justify-center items-center gap-[25px] inline-flex"></div>
+    </div>
+  );
+}
