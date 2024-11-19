94754180441cb458dc2cddfa71d5d9beb286b84b
Neil Doshi <mail.neildoshi@gmail.com>
2024-11-19 23:46:41 +0530

    feat: floor plan complete

--- src/components/floorplan/index.tsx
+++ src/components/floorplan/index.tsx
@@ -9,39 +9,39 @@ export default function FloorPlan({
 }: FloorPlanType) {
   return (
     <div className="flex flex-1 flex-col mt-6">
-      <div className="flex flex-1 flex-col py-8 gap-6 mx-auto lg:w-[1340px]">
+      <div className="flex flex-1 flex-col py-8 gap-6 xl:mx-auto 2xl:w-[1440px] xl:w-[1200px] lg:mx-2 mx-8">
         <Heading />
-        <div className="flex flex-1 gap-x-[3.75rem]">
-          <div className="flex-[2_2_0%] justify-center max-w-[44.375rem]">
-            <img src={floorPlan} className="max-w-[710px]" />
+        <div className="flex flex-1 xl:gap-x-[3.75rem] flex-col xl:flex-row mt-4 xl:mt-0">
+          <div className="xl:flex-[2_2_0%] flex-1 justify-center xl:max-w-[44.375rem]">
+            <img src={floorPlan} className="xl:max-w-[710px] w-full" />
           </div>
-          <div className="flex-[1_1_0%]">
+          <div className="xl:flex-[1_1_0%] flex-1">
             <FeatureList floor="1st" />
           </div>
         </div>
       </div>
-      <div className="flex flex-1 justify-center items-center mt-24 relative">
+      <div className="flex flex-1 justify-center items-center xl:mt-24 mt-4 relative">
         <img src="/chexbg.svg" className="absolute z-0" />
         <img
           src={floorElevation}
-          className="relative object-cover h-[25rem] z-10"
+          className="relative object-cover xl:h-[25rem] h-[15rem] z-10"
         />
       </div>
-      <div className="flex flex-1 flex-col py-8 gap-6 mt-24 mx-auto lg:w-[1340px]">
-        <div className="flex flex-1 gap-x-[3.75rem]">
-          <div className="flex-[1_1_0%]">
+      <div className="flex flex-1 flex-col py-8 gap-6 xl:mx-auto 2xl:w-[1440px] xl:w-[1200px] lg:mx-2 mx-8">
+        <div className="flex flex-1 xl:gap-x-[3.75rem] flex-col xl:flex-row mt-4 xl:mt-24">
+          <div className="xl:flex-[1_1_0%] flex-1">
             <FeatureList floor="2nd" />
           </div>
-          <div className="flex-[2_2_0%] justify-center max-w-[44.375rem]">
-            <img src={floorPlan} className="max-w-[710px]" />
+          <div className="xl:flex-[2_2_0%] flex-1 justify-center xl:max-w-[44.375rem]">
+            <img src={floorPlan} className="xl:max-w-[710px] w-full" />
           </div>
         </div>
       </div>
-      <div className="flex flex-1 justify-center items-center mt-24 relative">
+      <div className="flex flex-1 justify-center items-center xl:mt-24 mt-4 relative">
         <img src="/chexbg.svg" className="absolute z-0" />
         <img
           src={floorElevation}
-          className="relative object-cover h-[25rem] z-10"
+          className="relative object-cover xl:h-[25rem] h-[15rem] z-10"
         />
       </div>
     </div>
--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -7,7 +7,7 @@ type ProjectProps = {
 export default function Project({ images }: ProjectProps) {
   return (
     <div className="flex flex-1 justify-center mx-8">
-      <div className="flex flex-col xl:flex-row flex-1 py-8 gap-8  2xl:max-w-[1440px] xl:max-w-[1200px] mx-2 lg:mx-8 xl:gap-x-[48px]">
+      <div className="flex flex-col xl:flex-row flex-1 py-8 gap-8 2xl:max-w-[1440px] xl:max-w-[1200px] mx-2 lg:mx-8 xl:gap-x-[48px]">
         <div className="xl:flex-[2_2_0%] xl:max-w-[47.625rem]">
           <h4 className="font-lexend font-medium text-2xl leading-7">
             Project Images
