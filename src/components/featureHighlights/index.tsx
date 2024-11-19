494c34086af3c624d899b5f7d3219c01044de871
Neil Doshi <mail.neildoshi@gmail.com>
2024-11-19 22:55:19 +0530

    feat: make feature highlights responsive

--- src/components/featureHighlights/index.tsx
+++ src/components/featureHighlights/index.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 const features = [
   {
     image: "/features/earthquakeResistant.svg",
@@ -26,11 +24,17 @@ const features = [
 export default function FeatureHighlights() {
   return (
     <div className="flex flex-1 justify-center">
-      <div className="flex flex-1 justify-between py-8 max-w-[1340px]">
+      <div className="flex flex-1 justify-between py-8 2xl:max-w-[1440px] xl:max-w-[1200px] mx-2 lg:mx-8">
         {features.map((f, idx) => (
           <div key={idx} className="flex flex-col items-center gap-2">
-            <Image src={f.image} alt={f.label} width={81} height={81} />
-            <h4 className="font-lexend font-normal leading-7 text-center">{f.label}</h4>
+            <img
+              src={f.image}
+              alt={f.label}
+              className="h-[47px] w-[47px] xl:h[81px] xl:w-[81px]"
+            />
+            <h4 className="text-center text-[#232323] text-[9.50px] md:text-lg xl:text-xl font-normal font-lexend w-20 lg:w-32">
+              {f.label}
+            </h4>
           </div>
         ))}
       </div>
