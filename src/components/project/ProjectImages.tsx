2c38ba009ea55e2d94ce65466e0d0fe885516188
Neil Doshi <mail.neildoshi@gmail.com>
2024-11-19 23:32:05 +0530

    feat: make project details responsive

--- src/components/project/ProjectImages.tsx
+++ src/components/project/ProjectImages.tsx
@@ -1,28 +1,30 @@
+'use client';
 type ProjectImagesProps = { images: Array<string> };
 export default function ProjectImages({ images }: ProjectImagesProps) {
+  const isBelowMd = useIsBelowMd();
   return (
-    <div className="relative h-[496px] max-w-[762px] mt-4">
+    <div className="relative w-[378px] h-[246.32px] lg:h-[496px] lg:w-[900px] md:w-[750px] mt-4">
       <img
         src={images[0]}
         alt="Hero Image"
-        className="absolute inset-0 object-cover h-[496px] w-[762px] rounded-lg z-0"
+        className="absolute inset-0 object-cover w-[378px] h-[246.32px] lg:h-[496px] lg:w-[900px] md:w-[750px] md:h-[340px] rounded-lg z-0"
       />
-      <div className="flex flex-1 gap-4 h-full w-full justify-center items-end z-10 translate-y-14">
-        {images.slice(0, 3).map((img, idx) => (
+      <div className="flex flex-1 gap-4 h-full w-full justify-center items-end z-10  translate-y-8  md:translate-y-32 lg:translate-y-14">
+        {images.slice(0, isBelowMd ? 2 : 3).map((img, idx) => (
           <img
             src={img}
-            className="rounded-md  h-[127px] w-[163px] border-white-100 border-2 rounded-md"
+            className="rounded-md  w-[102.68px] h-20 lg:h-[127px] lg:w-[163px] border-white-100 border-2 rounded-md"
             key={idx}
           />
         ))}
-        <div className="relative h-[127px] w-[163px] -translate-y-0 ">
+        <div className="relative w-[102.68px] h-20 lg:h-[127px] lg:w-[163px] -translate-y-0 ">
           <img
             src={images[3]}
-            className=" absolute inset-0 rounded-md  h-[127px] w-[163px] border-white-100 border-2 rounded-md"
+            className=" absolute inset-0 rounded-md  w-[102.68px] h-20 lg:h-[127px] lg:w-[163px] border-white-100 border-2 rounded-md"
           />
           <div className="absolute inset-0 bg-black-100/50 border-white-100 border-2 rounded-md" />
           <div className="absolute inset-0 flex flex-1 justify-center items-center">
-            <p className="font-lexend text-[20.04px] text-center text-white-100 w-16 font-medium leading-[21.83px]">
+            <p className="font-lexend text-xs lg:text-[20.04px] text-center text-white-100 w-16 font-medium leading-[13.67px] lg:leading-[21.83px]">
               View more(7)
             </p>
           </div>
@@ -30,4 +32,18 @@ export default function ProjectImages({ images }: ProjectImagesProps) {
       </div>
     </div>
   );
+}
+
+import { useState, useEffect } from 'react';
+
+const useIsBelowMd = () => {
+  const [isBelowMd, setIsBelowMd] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsBelowMd(window.innerWidth < 768);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isBelowMd;
 }
--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -5,17 +5,18 @@ type ProjectProps = {
   images: Array<string>;
 };
 export default function Project({ images }: ProjectProps) {
-
   return (
     <div className="flex flex-1 justify-center mx-8">
       <div className="flex flex-col xl:flex-row flex-1 py-8 gap-8  2xl:max-w-[1440px] xl:max-w-[1200px] mx-2 lg:mx-8 xl:gap-x-[48px]">
-        <div className="xl:flex-[2_2_0%] max-w-[47.625rem]">
+        <div className="xl:flex-[2_2_0%] xl:max-w-[47.625rem]">
           <h4 className="font-lexend font-medium text-2xl leading-7">
             Project Images
           </h4>
-          <ProjectImages images={images} />
+          <div className="flex flex-1 justify-center">
+            <ProjectImages images={images} />
+          </div>
         </div>
-        <div className="xl:flex-[1_1_0%] mt-20 xl:mt-0">
+        <div className="xl:flex-[1_1_0%] lg:mt-20 md:mt-24 xl:mt-0">
           <h4 className="font-lexend font-medium text-2xl leading-7">
             Project Details
           </h4>
